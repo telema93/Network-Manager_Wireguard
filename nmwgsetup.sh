188a7bbf5ccc24bf34b694192f6518fd2c0ec0ea
Telema <31537127+telema93@users.noreply.github.com>
2020-08-21 12:43:18 +0300

    Update nmwgsetup.sh

--- nmwgsetup.sh
+++ nmwgsetup.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 echo -n "Enter your sudo passord: "
 read supassw
-echo $supassw | sudo -S apt install wireguard git dh-autoreconf libglib2.0-dev intltool build-essential libgtk-3-dev libnma-dev libsecret-1-dev network-manager-dev resolvconf
+echo $supassw | sudo -S apt update
+sudo apt upgrade -y
+sudo apt install wireguard git dh-autoreconf libglib2.0-dev intltool build-essential libgtk-3-dev libnma-dev libsecret-1-dev network-manager-dev resolvconf
 git clone https://github.com/max-moser/network-manager-wireguard
 cd network-manager-wireguard
 ./autogen.sh --without-libnm-glib
 ./configure --without-libnm-glib --prefix=/usr --sysconfdir=/etc --libdir=/usr/lib/x86_64-linux-gnu --libexecdir=/usr/lib/NetworkManager --localstatedir=/var
-echo $supassw | sudo -S make install
-echo $supassw | sudo -S systemctl restart NetworkManager.service
+sudo make install
+sudo systemctl restart NetworkManager.service
 echo -e "\e[32mInstallation completed\e[0m"
